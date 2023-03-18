905bfd00e3f89905e83d5efeadb6f60d03863a28
takeshi31 <heber87@gmail.com>
2023-03-18 00:08:06 -0500

    refactor: Clear code

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,3 @@
-// import express from 'express'
-// import processTextRequest from './repositories/processTextRequest'
-// import cors from 'cors'
-
-// import textsRoutes from './routes/texts'
-
-// const app = express()
-// const PORT: string = '3000'
-
-// app.locals.storageText = []
-
-// app.use(express.json())
-
-// // app.use(cors)
-
-// // app.use('/add-text', textsRoutes)
-// app.get('/add-text', cors(), (req: express.Request, res: express.Response) => {
-//   const textInput = req.query.text
-
-//   if (typeof textInput !== 'string') {
-//     res.status(400).send({ message: 'Type input invalid' })
-//   }
-
-//   res.send(processTextRequest(textInput as string))
-// })
-
-// app.listen(PORT, () => { })
-
-// export default app
-
 import express, { Application } from 'express'
 import cors from 'cors'
 
@@ -44,6 +14,4 @@ const textController = new TextController(textStorage)
 
 app.get('/add-text', textController.addText.bind(textController))
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000')
-})
+app.listen(3000, () => {})
