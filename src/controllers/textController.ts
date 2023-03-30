355bbb1a13e47240030f8e3e4cb623458c79b3ea
takeshi31 <heber87@gmail.com>
2023-03-30 01:05:20 -0500

    feat: Add swagger

--- src/controllers/textController.ts
+++ src/controllers/textController.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express'
+import { IError } from '../interfaces/error.interface'
+import { ITextInput } from '../interfaces/textInput.interface'
 import { TextStorage } from '../repositories/textStorage'
 
 interface Input {
@@ -18,17 +20,25 @@ export class TextController {
     this.textStorage = textStorage
   }
 
-  async addText (req: Request<{}, {}, {}, Input>, res: Response): Promise<void> {
-    const { text, page, size } = req.query
+  async addText (req: Request<{}, {}, Input>, res: Response): Promise<void> {
+    const { text, page, size } = req.body
+    const response: IError | ITextInput[] = await this.textStorage.addText(text, page, size)
 
-    const response = await this.textStorage.addText(text, page, size)
-    res.send(response)
+    if (typeof response === 'object' && response !== null && 'code' in response) {
+      res.status(response?.code).send(response)
+    }
+
+    if (response instanceof Array) res.send(response)
   }
 
   async deleteText (req: Request<DeleteText, {}, {}, {}>, res: Response): Promise<void> {
     const { id } = req.params
-    console.log(`ID => ${id}`)
-    const result = await this.textStorage.delete(id)
-    res.send(result)
+    const result: IError | ITextInput[] = await this.textStorage.delete(id)
+
+    if (typeof result === 'object' && result !== null && 'code' in result) {
+      res.status(result?.code).send(result)
+    }
+
+    if (result instanceof Array) res.send(result)
   }
 }
